6b4faf1b60d1be933922dbc01f8d54e78c98187c
tao.xu <tao.xu2008@outlook.com>
2020-11-05 11:16:49 +0800

    fs_di

---
+++ pkgs/ltp/create/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+@file  : __init__.py.py
+@Time  : 2020/11/5 11:15
+@Author: Tao.Xu
+@Email : tao.xu2008@outlook.com
+"""
+
+"""
+./create_datafile -h
+=====================
+usage:
+    create_file <# of 1048576 buffers to write> <name of file to create>
+     ex. # create_file 10 /tmp/testfile
+"""
+
+if __name__ == '__main__':
+    pass
