b551334589a95f02a64844313c8c24c442bdd4a2
tao.xu <tao.xu2008@outlook.com>
2020-11-10 09:30:50 +0800

    1.0.1 supports tests on mount point

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.4+',
+        'Programming Language :: Python :: 3.9.0',
     ],
 )
